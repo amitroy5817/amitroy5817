a91128a0c0e33a9d72d5db52920dfa1a7bdfec5e
Amit Roy <amitroy5817@gmail.com>
2024-07-20 14:19:17 +0530

    my portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import picture from './display-picture.png';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [height, setHeight] = useState(80); // Initial height of the arrow
+  const [viewportHeight, setviewportHeight] = useState();
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const viewportHeight = window.innerHeight;
+    setviewportHeight(viewportHeight);
+    const newHeight = 80 + (scrollY / documentHeight) * viewportHeight;
+    setHeight(newHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <ul>
+        <li><a href="#about"><span>01.</span>about</a></li> 
+        <li><a href="#projects"><span>02.</span>projects</a></li> 
+        <li><a href="#skills"><span>03.</span>skills</a></li> 
+        <li><a href="#contact"><span>04.</span>contact</a></li>
+      </ul>
       </header>
+      <section id='hero'>
+        <h2>Amit Roy</h2><br />
+        <h3>Frontend Developer</h3>
+        <div className="connect-social">
+          Connect with me
+          <ul>
+            <li><a href="https://github.com/amitroy-thedev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg></a></li>
+            {/* <li><a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg></a></li> */}
+            <li><a href="https://linkedin.in/in/amitroy-thedev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg></a></li>
+          </ul>
+        </div>
+        <div style={{position: "fixed", bottom: "10px", right: "0", display: height >= viewportHeight ? "none" : "block"}}>
+        <svg width="24" height={height} viewBox={`0 0 24 ${height}`} data-name="Flat Line" xmlns="http://www.w3.org/2000/svg" className="icon flat-line">
+          <path style={{ fill: 'none', stroke: '#000', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: 2 }} d={`M12 3V${height - 10}`} />
+          <path data-name="primary" style={{ fill: 'none', stroke: '#000', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: 2 }} d={`M19 ${height - 10}l-7 7-7-7`} />
+        </svg>
+        </div>
+
+      </section>
+      <section id='about'>
+      <h2 className='section-heading'><span>01. </span>about</h2>
+      <img src={picture} alt="display picture" />
+      <p>Hello ! <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M40.1 467.1l-11.2 9c-3.2 2.5-7.1 3.9-11.1 3.9C8 480 0 472 0 462.2L0 192C0 86 86 0 192 0S384 86 384 192l0 270.2c0 9.8-8 17.8-17.8 17.8c-4 0-7.9-1.4-11.1-3.9l-11.2-9c-13.4-10.7-32.8-9-44.1 3.9L269.3 506c-3.3 3.8-8.2 6-13.3 6s-9.9-2.2-13.3-6l-26.6-30.5c-12.7-14.6-35.4-14.6-48.2 0L141.3 506c-3.3 3.8-8.2 6-13.3 6s-9.9-2.2-13.3-6L84.2 471c-11.3-12.9-30.7-14.6-44.1-3.9zM160 192a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm96 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg><br/>
+      My name is Amit. I am a Software Engineering student at GCELT (Government College of Engineering and Leather Technology), 
+      I am good at frontend development and I am also interested to explore Full Stack Development. My interest in Development 
+      started back when i was in my 8th standard, when the android was completely new for me.</p><p>Interested in Creating Things.</p><p>Not a Professional but a Passionate.</p>
+      <p>Currently working @ nowhere</p>
+      <p><a href='https://drive.google.com/file/d/1tRviVOT3NxLErK4p8ia4O5OHZ_qEQn9t/view?usp=sharing' className='download-cv'>Download CV</a></p>
+      </section>
+      <section id='projects'>
+      <h2 className='section-heading'><span>02. </span>projects</h2>
+      <div className="project">
+      <h3><a href='https://notboook.netlify.app' target='_blank'>notboook <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z"/></svg></a></h3>
+      <p>A website to host all your notes, important questions, note snaps
+which can be easily shared to your friends through generated Qr
+code. Built using <b>ReactJS</b> and <b>Firebase</b>
+<ul className='features'>
+  <li><b>Some Key Features</b> </li>
+  <li>Share your notes with generated unique ID or QR Code ✔️</li>
+  <li>Explore others public notes ✔️</li>
+  <li>Night / Day Mode ✔️</li>
+  <li>Listen your notes with the help of tts functionality ✔️</li>
+  <li>full control of your notes with CRUD functionality ✔️</li>
+  <li>Download your notes ( in .txt file format) ✔️</li>
+</ul>
+</p></div>
+      </section>
+      <section id='skills'>
+      <h2 className='section-heading'><span>03. </span>skills</h2>
+      <p>Here are few technologies i’ve been into
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>ReactJS</li>
+          <li>C lang</li>
+          <li>Java</li>
+        </ul>
+      </p>
+      </section>
+      <section id='contact'>
+      <h2 className='section-heading'><span>04. </span>contact</h2>
+      <div className="contact-details">
+        <p>I'm constantly looking for new oppurtunities so my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
+        <a href="mailto:amitroy.thedev@gmail.com?subject=Hello" class="email-button">Say Hello<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376l0 103.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg></a>
+      </div>
+      </section>
     </div>
   );
 }
